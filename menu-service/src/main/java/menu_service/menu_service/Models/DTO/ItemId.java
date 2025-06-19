27335fa1c93aa4a370d853fb28682c5d107530bd
AngelZlatkov91 <128607549+AngelZlatkov91@users.noreply.github.com>
2025-06-19 17:10:45 +0300

    fix bug with itemId Controller to get item with String id and delete item

---
+++ menu-service/src/main/java/menu_service/menu_service/Models/DTO/ItemId.java
@@ -0,0 +1,13 @@
+package menu_service.menu_service.Models.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ItemId {
+    private String ItemId;
+
+}
